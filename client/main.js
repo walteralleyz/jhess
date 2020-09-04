679423b020da0eb46cbb48dee3fdeba9738c0178
walteralleyz <walterdasilvasantos@gmail.com>
2020-09-04 19:29:48 -0300

    some move logic added

--- client/main.js
+++ client/main.js
@@ -15,22 +15,27 @@ class Game {
         "rook",
     ];
     matrix = [
-        Array.from(this.pieces, (piece) => "w-" + piece),
-        Array(8).fill("w-pawn"),
+        Array.from(this.pieces, (piece) => "y-" + piece),
+        Array(8).fill("y-pawn"),
         Array(8).fill(0),
         Array(8).fill(0),
         Array(8).fill(0),
         Array(8).fill(0),
-        Array(8).fill("b-pawn"),
-        Array.from(this.pieces, (piece) => "b-" + piece),
+        Array(8).fill("r-pawn"),
+        Array.from(this.pieces, (piece) => "r-" + piece),
     ];
 
-    player = "w";
+    player = "y";
     selected = {
         color: "",
         piece: "",
         row: 0,
         col: 0,
+    };
+
+    killZone = {
+        y: [],
+        r: [],
     };
 
     setup() {
@@ -59,28 +64,80 @@ class Game {
         }
     }
 
-    showMessage() {
-        const messageBox = document.querySelector('span');
+    showMessage(message) {
+        const messageBox = document.querySelector("span");
 
-        messageBox.textContent = `Player ${this.player.toUpperCase()} now!`;
+        if(message) {
+            messageBox.textContent = message;
+            messageBox.style.backgroundColor = "gray";    
+        } else {
+            messageBox.textContent = `Player ${this.player.toUpperCase()} now!`;
+            messageBox.style.backgroundColor = this.player === "y" ? "gold" : "red";
+        }
     }
 
     select(e) {
-        const block = e.currentTarget;
-        const row = block.parentNode;
-        const piece = block.childNodes[0] || undefined;
+        if (!this.gameEnd) {
+            const block = e.currentTarget;
+            const row = block.parentNode;
+            const piece = block.childNodes[0] || undefined;
 
-        if (piece) {
-            const [color, vendor, chess] = piece.classList.value.split(" ");
+            if (piece) {
+                const [color, vendor, chess] = piece.classList.value.split(" ");
 
-            if (color == this.player) {
-                this.selected.color = color;
-                this.selected.piece = chess.split("-")[2];
-                this.selected.col = block.getAttribute("index");
-                this.selected.row = row.getAttribute("index");
+                if (color == this.player) {
+                    this.selected.color = color;
+                    this.selected.piece = chess.split("-")[2];
+                    this.selected.col = block.getAttribute("index");
+                    this.selected.row = row.getAttribute("index");
+                } else {
+                    this.drop(e);
+                }
+            } else {
+                this.drop(e);
             }
-        } else {
-            this.drop(e);
+        } else console.log("teste");
+    }
+
+    clearBlock(row, block) {
+        this.matrix[this.selected.row][this.selected.col] = 0;
+        this.matrix[row.getAttribute("index")][block.getAttribute("index")] =
+            this.selected.color + "-" + this.selected.piece;
+    }
+
+    kill(piece) {
+        this.killZone[this.player] = [...this.killZone[this.player], piece];
+        this.updateKillZone();
+    }
+
+    updateKillZone() {
+        const yKillZone = document.querySelector("#kill-zone-y .pieces");
+        const rKillZone = document.querySelector("#kill-zone-r .pieces");
+
+        this.clearKillZone(yKillZone);
+        this.clearKillZone(rKillZone);
+
+        for (let p of this.killZone.y) {
+            const span = document.createElement("span");
+            span.classList.add("mdi", p);
+            yKillZone.appendChild(span);
+        }
+
+        for (let p of this.killZone.r) {
+            const span = document.createElement("span");
+            span.classList.add("mdi", p);
+            rKillZone.appendChild(span);
+        }
+    }
+
+    clearKillZone(zone) {
+        zone.innerHTML = "";
+    }
+
+    isItKing(piece) {
+        if (piece.indexOf("king") !== -1) {
+            this.gameEnd = true;
+            this.showMessage("Game is end!");
         }
     }
 
@@ -90,21 +147,28 @@ class Game {
 
         const child = block.childNodes[0] || undefined;
 
-        if (
-            (this.selected.piece && !block.childElementCount) ||
-            (child && child.classList.value.indexOf(this.player) === -1)
-        ) {
-            this.matrix[this.selected.row][this.selected.col] = 0;
-            this.matrix[row.getAttribute("index")][
-                block.getAttribute("index")
-            ] = this.selected.color + "-" + this.selected.piece;
+        if (this.selected.piece) {
+            this.clearBlock(row, block);
 
-            this.changePlayer();
+            if (!block.childElementCount) {
+                this.changePlayer();
+            } else {
+                if (
+                    child &&
+                    child.classList.value.indexOf(this.player) === -1
+                ) {
+                    const piece = child.classList.value.split(" ")[2];
+
+                    this.kill(piece);
+                    this.changePlayer();
+                    this.isItKing(piece);
+                }
+            }
         }
     }
 
     changePlayer() {
-        this.player = this.player == "w" ? "b" : "w";
+        this.player = this.player == "y" ? "r" : "y";
         this.selected = {
             color: "",
             piece: "",
